99a85edbef677fa2515961635ebd4373983e7195
TJ Stevens <tj@tjstevens.net>
2012-12-07 16:40:14 -0500

    added thanks page

--- app.rb
+++ app.rb
@@ -6,10 +6,9 @@ enable :sessions
 get '/' do
     @community = params[:community]
     erb :terms
-    #, :locals => {:community => params[:community]}
 end
 
-#post '/form' do
-#    redirect "https://covergirl-demo.force.com/"
-#                #+ #{params[:message] + '/login')
-#end
+get '/exit' do
+    @community = params[:community]
+    erb :exit
+end

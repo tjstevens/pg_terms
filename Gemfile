b2f0d6d241c8d64beb76f1f20440a78e6818b3f9
TJ Stevens <tj@tjstevens.net>
2012-12-10 22:49:46 -0500

    swapped data model for cookie-based solution

--- Gemfile
+++ Gemfile
@@ -3,6 +3,3 @@ source :rubygems
 
 gem 'heroku'
 gem 'sinatra'
-gem 'data_mapper'
-gem 'pg'
-gem 'dm-postgres-adapter'
--- app.rb
+++ app.rb
@@ -1,46 +1,43 @@
-require 'rubygems'
+#require 'rubygems'
 require 'sinatra'
-require 'data_mapper'
-require File.dirname(__FILE__) + '/models.rb'
-#require 'dm-sqlite-adapter'
-#require 'dm-postgres-adapter'
 
-enable :sessions
+#enable :sessions
 
 get '/' do
+    # Grab community URL from inbound request so we can pass it to the view
     @community = params[:community]
-    
-    @result = Responses.first(:url => @community)
-    if not @result.nil?
-        if @result.complete == true
-            #"redirecting to " + @community
-            redirect @community
-        else
-            erb :terms
-        end
+
+    # See if there is a covergirl cookie in the browser
+    @cookie = request.cookies["covergirl"]
+
+    # If we find one, we know they previously accepted the T&Cs so send them along otherwise send them to the T&Cs page
+    if @cookie == @community
+        redirect @community
     else
-        @result = Responses.create(:url => @community, :complete => false)
-        @result.save
-        #STDERR.puts @result.errors.inspect
-        erb :terms
+		erb :terms
     end
 end
 
 get '/continue' do
+	# This page is for users returning from ACCEPTING the T&Cs
+	# Like always, grab the community URL from the inbound request so we can pass them along when we are done
     @community = params[:community]
-    @result = Responses.first(:url => @community)
-    @result.complete = true
-    @result.save
-    #STDERR.puts @result.errors.inspect
-    #"redirecting to " + @community
-    redirect @community
-end
 
-get '/db' do
-    @responses = Responses.all
+    # Setup the expiration variable that will go into the cookie... we're shooting for about a month
+    @expiration = Time.now + (60 * 60 * 24 * 30)
+
+    # Stamp the cookie with the community URL and expiration date
+	response.set_cookie("covergirl", :value => @community, :expires => @expiration)
+
+	# ... and off you go!
+	redirect @community
 end
 
 get '/exit' do
+	# This pace is for users returning from DECLINING the T&Cs
+	# Again, grab the community URL from the inbound request so we can pass it to the view... just in case they change their mind
     @community = params[:community]
+
+    # ... and you are outta here!
     erb :exit
 end
